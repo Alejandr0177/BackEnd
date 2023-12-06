31d294a354a653308de449f3ceca3436ec38bdcd
Supadre18 <je.pachecomartinez@ugto.mx>
2023-12-05 18:26:02 -0600

    Ya actualiza el usuario

--- routes/patients.js
+++ routes/patients.js
@@ -17,7 +17,7 @@ router.get('/showPats', async (req, res) => {
 router.post('/registerPat', async (req, res) => {
     const body = req.body;
     const {name, lastname, email, phone, birth, gender, treatment, bloodgroup} = req.body;
-    const sql = "INSERT INTO patients(pat_name, pat_lastname, pat_email, pat_phone, pat_birth, pat_gender, pat_treatment, pat_bloodgroup) VALUES (?) ";
+    const sql = "INSERT INTO patients(pat_name, pat_lastname , pat_email, pat_phone, pat_birth, pat_gender, pat_treatment, pat_bloodgroup) VALUES (?) ";
     const values = [name, lastname, email, phone, birth, gender, treatment, bloodgroup];
     connection.query(sql, [values], function (err, result) {
         if (err) throw err;
@@ -28,5 +28,33 @@ router.post('/registerPat', async (req, res) => {
         });
     });
 })
+
+router.post('/updatePat/:id', async (req, res) => {
+    const { id } = req.params; // Obtener el ID del paciente de los parámetros de la URL
+    const { name, lastname, email, phone, birth, gender, treatment, bloodgroup } = req.body;
+
+    const sql = `
+        UPDATE patients 
+        SET pat_name = ?, pat_lastname = ?, pat_email = ?, pat_phone = ?, pat_birth = ?, pat_gender = ?, pat_treatment = ?, pat_bloodgroup = ?
+        WHERE id_pat= ?`; // Se asume que 'pat_id' es el campo que representa el ID del paciente en la base de datos
+
+    const values = [name, lastname, email, phone, birth, gender, treatment, bloodgroup, id];
+
+    connection.query(sql, values, function (err, result) {
+        if (err) {
+            console.error("Error al actualizar paciente:", err);
+            res.status(500).json({
+                error: 'Error al actualizar paciente',
+                data: null
+            });
+        } else {
+            console.log("Información del paciente actualizada");
+            res.json({
+                error: null,
+                data: result
+            });
+        }
+    });
+});
 
 module.exports = router;
