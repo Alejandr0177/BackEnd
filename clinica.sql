64884249b44f3e8ddaef27bc7b63fbdb2e02fcdb
Alejandro <a.garciagarcia7@ugto.mx>
2023-12-07 03:35:25 -0600

    base da datos modificada

--- clinica.sql
+++ clinica.sql
@@ -34,26 +34,16 @@ UNIQUE (pat_phone)
 
 CREATE TABLE IF NOT EXISTS appointment(
 app_id INT NOT NULL AUTO_INCREMENT,
+app_name VARCHAR(30) NOT NULL,
+app_email VARCHAR(50) NOT NULL,
+app_phone VARCHAR(10) NOT NULL,
 app_date DATE NOT NULL,
 app_hour TIME NOT NULL,
-app_doc_id INT NOT NULL,
-app_pat_id INT NOT NULL,
 
 PRIMARY KEY (app_id),
 UNIQUE(app_date , app_hour),
 INDEX(app_date),
 
-CONSTRAINT fk_doctor_appointment
-		FOREIGN KEY (app_doc_id)
-        REFERENCES doctors (doc_id)
-        ON DELETE RESTRICT
-        ON UPDATE RESTRICT,
-        
-CONSTRAINT fk_patient_appointment
-		FOREIGN KEY (app_pat_id)
-        REFERENCES patients (pat_id)
-        ON DELETE RESTRICT
-        ON UPDATE RESTRICT
 );
 
 INSERT INTO doctors(doc_name , doc_app , doc_apm , doc_email , doc_password , doc_phone)
